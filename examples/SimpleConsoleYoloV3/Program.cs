2a15867555cc44cd65abf99ec692e1a108600f33
Denis Pettens <denis.pettens@gmail.com>
2018-11-23 18:52:11 +0100

    Update simple console example with the new changes

--- examples/SimpleConsoleYoloV3/Program.cs
+++ examples/SimpleConsoleYoloV3/Program.cs
@@ -1,35 +1,35 @@
-﻿using OpenCvSharp;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
+using OpenCvSharp;
 using YoloCSharp;
 
-namespace YoloCSharpTest
+namespace SimpleConsoleYoloV3
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             // Initialise darknet with official yolov3 model
-            Darknet darknet = new Darknet("data/yolov3.cfg", "data/yolov3.weights");
-
-            // Load all classnames to make the relationship between yolov3 model id and real names of objects
-            var classNames = File.ReadAllLines("data/coco.names");
-
-            // Open dog.jpg and convert it to an OpenCV Mat Object
-            Mat image = new Mat("data/dog.jpg");
-
-            // Try to detect all objects that model know with a thresh of 30%
-            List<NetResult> results = darknet.Detect(image, 0.3f);
-
-            // Print all results found by yolo
-            foreach (var result in results)
+            using (var darknet = new Darknet("data/yolov3.cfg", "data/yolov3.weights"))
             {
-                Console.WriteLine($"{classNames[result.ObjId]} - {result.Prob * 100}%");
-            }
+                // Load all classnames to make the relationship between yolov3 model id and real names of objects
+                var classNames = File.ReadAllLines("data/coco.names");
 
-            Console.WriteLine("Press any key to quit the example :");
-            Console.ReadLine();
+                // Open dog.jpg and convert it to an OpenCV Mat Object
+                var image = new Mat("data/dog.jpg");
+
+                // Try to detect all objects that model know with a thresh of 30%
+                var results = darknet.Detect(image, 0.3f);
+
+                // Print all results found by yolo
+                foreach (var result in results)
+                {
+                    Console.WriteLine($"{classNames[result.ObjId]} - {result.Prob * 100}%");
+                }
+
+                Console.WriteLine("Press any key to quit the example :");
+                Console.ReadLine();
+            }
         }
     }
 }
