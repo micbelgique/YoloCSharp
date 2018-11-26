51ee26b8ca791af3e276d252a74e95945e8bf282
Denis Pettens <denis.pettens@gmail.com>
2018-11-26 23:41:50 +0100

    Add simple console example for .Net Framework 4.6.1

---
+++ examples/SimpleConsoleNetFramework/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using OpenCvSharp;
+using YoloCSharp;
+
+namespace SimpleConsoleNetFramework
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Initialise darknet with official yolov3 model
+            using (var darknet = new Darknet("data/yolov3.cfg", "data/yolov3.weights"))
+            {
+                // Load all classnames to make the relationship between yolov3 model id and real names of objects
+                var classNames = File.ReadAllLines("data/coco.names");
+
+                // Open dog.jpg and convert it to an OpenCV Mat Object
+                var image = new Mat("data/dog.jpg");
+
+                // Try to detect all objects that model know with a thresh of 30%
+                var results = darknet.Detect(image, 0.3f);
+
+                // Print all results found by yolo
+                foreach (var result in results)
+                {
+                    Console.WriteLine($"{classNames[result.ObjId]} - {result.Prob * 100}%");
+                }
+
+                Console.WriteLine("Press any key to quit the example :");
+                Console.ReadLine();
+            }
+        }
+    }
+}
